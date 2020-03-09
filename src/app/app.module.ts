cc395ffb681469662656fc97bf701237bb3bdb8a
Jean Breitner Mora Galvis <Jean.Mora@OMNICON.CC>
2020-03-09 13:41:36 -0500

    Modificacion modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,11 @@ import { NgModule } from '@angular/core';
 //RUTAS
 import { APP_ROUTES } from './app.routes';
 
-import { AppRoutingModule } from './app-routing.module';
+// Modulos
+import { PagesModule } from './pages/pages.module';
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
 
 
@@ -23,21 +17,13 @@ import { RegisterComponent } from './login/register.component';
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Grafica1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    PagesComponent
-
 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    APP_ROUTES
+    APP_ROUTES,
+    PagesModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,28 +1,10 @@
 import { RouterModule,Routes } from "@angular/router";
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
+
 import { LoginComponent } from './login/login.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
 
 const appRoutes:Routes = [
-    {
-        path:'',
-        component: PagesComponent,
-        children: [
-            {path:'dashboard', component: DashboardComponent},
-            {path:'progress', component: ProgressComponent},    
-            {path:'grafica1', component: Grafica1Component},
-            //Cuando detecta un caracter vacia lo redirecciona
-            {path:'', redirectTo:'/dashboard', pathMatch:'full'}
-
-        ]
-    
-    
-    },
-
 
     {path:'login', component: LoginComponent},
     
